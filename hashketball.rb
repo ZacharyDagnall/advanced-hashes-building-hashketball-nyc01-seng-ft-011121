5a55b1af285a35481544a8c900a16fe3facccecc
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-16 17:35:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,115 @@
-# Write your code here!
+def game_hash
+  game = {home: {
+                team_name: 'Brooklyn Nets',
+                colors: ['Black', 'White'],
+                players: [{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  }]
+                },      
+          away: {
+                team_name: 'Charlotte Hornets',
+                colors: ['Turquoise', 'Purple'],
+                players: [{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  },{
+                  player_name: '',
+                  number: '',
+                  shoe: '',
+                  points: -1,
+                  rebounds: -1,
+                  assists: -1,
+                  steals: -1,
+                  blocks: -1,
+                  slam_dunks: -1
+                  }]
+                }
+          }
+  
+  game
+end
